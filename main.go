83d666c9d803ad125e06520f6699041fa44dbe81
Johnw7789 <74395844+Johnw7789@users.noreply.github.com>
2024-12-05 02:38:00 -0500

    Delete main.go

--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func main() {
-	// tests.TestReserveHME()
-	// tests.TestRetrieveHMEList()
-	// tests.TestDeactivateHME()
-	// tests.TestReactivateHME()
-	// tests.TestDeleteHME()
-}
